3ade67ebda250eedaae0fe1a9264f7e56736aa89
Khagesh Sharma <khageshhiet@gmail.com>
2021-09-14 17:37:42 +0530

    Ask for push notification permission if not already given

--- src/physical-id/physical-id-screen.js
+++ src/physical-id/physical-id-screen.js
@@ -1,11 +1,14 @@
 // @flow
 
-import React, {
-  useEffect,
-  useState,
-  useMemo
-} from 'react'
-import { StyleSheet, Text, View, Platform, Image, DeviceEventEmitter } from 'react-native'
+import React, { useEffect, useState, useMemo, useRef } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Image,
+  DeviceEventEmitter,
+} from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigation, useIsFocused } from '@react-navigation/native'
 import CountryPicker from 'react-native-country-picker-modal'
@@ -34,8 +37,15 @@ import {
   physicalIdConnectionStatus,
 } from './physical-id-type'
 import { physicalIdRoute } from '../common/route-constants'
-import { homeDrawerRoute, homeRoute, physicalIdSuccessRoute } from '../common'
+import {
+  homeDrawerRoute,
+  homeRoute,
+  physicalIdSuccessRoute,
+  pushNotificationPermissionRoute,
+} from '../common'
 import { ModalButtons } from '../components/buttons/modal-buttons'
+import { getPushNotificationAuthorizationStatus } from '../push-notification/components/push-notification-permission-screen'
+import { usePushNotifications } from '../external-imports'
 
 // import { physicalIdHeadline } from '../external-imports'
 
@@ -54,6 +64,7 @@ function PhysicalId() {
   const [loaderText, setLoaderText] = useState(LOADER_TEXT.preparation)
   const [countryPickerVisible, setCountryPickerVisible] = useState(false)
   const focus = useIsFocused()
+  const actedOnPushPermission = useRef()
 
   const resetState = () => {
     setCountry()
@@ -77,7 +88,7 @@ function PhysicalId() {
     if (Platform.OS === 'android') {
       DeviceEventEmitter.addListener('FACE_SCAN', () => {
         setLoaderText(LOADER_TEXT.finish)
-      });
+      })
     }
     return () => {
       if (Platform.OS === 'android') {
@@ -86,7 +97,21 @@ function PhysicalId() {
     }
   }, [])
 
-  const onAction = () => {
+  const onAction = async () => {
+    const isAuthorized = await getPushNotificationAuthorizationStatus()
+    if (
+      Platform.OS === 'ios' &&
+      usePushNotifications &&
+      !isAuthorized &&
+      !actedOnPushPermission.current
+    ) {
+      navigation.navigate(pushNotificationPermissionRoute, {
+        intendedRoute: physicalIdRoute,
+      })
+      actedOnPushPermission.current = true
+      return
+    }
+
     if (!!country && !!document) {
       dispatch(launchPhysicalIdSDK(country, document))
       resetState()
@@ -127,11 +152,14 @@ function PhysicalId() {
     setLoaderText(getLoaderMessageText(processStatus))
   }, [processStatus])
 
-  const loaderWithMessage = useMemo(() => (
-    <Container tertiary key={loaderText}>
-      <Loader showMessage={true} message={loaderText} />
-    </Container>
-  ), [processStatus, loaderText])
+  const loaderWithMessage = useMemo(
+    () => (
+      <Container tertiary key={loaderText}>
+        <Loader showMessage={true} message={loaderText} />
+      </Container>
+    ),
+    [processStatus, loaderText]
+  )
 
   if (isLoaderVisible(processStatus, connectionStatus)) {
     return loaderWithMessage
@@ -155,42 +183,40 @@ function PhysicalId() {
         ) : null}
       </Container>
       <CustomView row safeArea>
-        {
-          !country ?
-            <ModalButtons
-              onPress={onAction}
-              onIgnore={onCancel}
-              colorBackground={colors.main}
-              acceptBtnText="Start Document Verification"
-              topTestID={`${testID}-start`}
-              containerStyles={styles.actionContainer}
-            />:
-            <ModalButtons
-              onPress={onAction}
-              onIgnore={onCancel}
-              colorBackground={colors.main}
-              secondColorBackground={colors.main}
-              denyButtonText="Cancel"
-              acceptBtnText="Scan Document"
-              disableAccept={country && !document}
-              topTestID={`${testID}-continue`}
-              containerStyles={styles.actionContainer}
-            />
-        }
+        {!country ? (
+          <ModalButtons
+            onPress={onAction}
+            onIgnore={onCancel}
+            colorBackground={colors.main}
+            acceptBtnText="Start Document Verification"
+            topTestID={`${testID}-start`}
+            containerStyles={styles.actionContainer}
+          />
+        ) : (
+          <ModalButtons
+            onPress={onAction}
+            onIgnore={onCancel}
+            colorBackground={colors.main}
+            secondColorBackground={colors.main}
+            denyButtonText="Cancel"
+            acceptBtnText="Scan Document"
+            disableAccept={country && !document}
+            topTestID={`${testID}-continue`}
+            containerStyles={styles.actionContainer}
+          />
+        )}
       </CustomView>
     </Container>
   )
 }
 
 const LOADER_TEXT = {
-  preparation: "Please have your document ready",
-  finish: "Finishing up...",
-  processing: "Processing..."
+  preparation: 'Please have your document ready',
+  finish: 'Finishing up...',
+  processing: 'Processing...',
 }
 
-const LoaderVisiblePhysicalIdStates = [
-  physicalIdProcessStatus.SDK_SCAN_START,
-]
+const LoaderVisiblePhysicalIdStates = [physicalIdProcessStatus.SDK_SCAN_START]
 
 const LoaderVisiblePhysicalIdConnectionStates = [
   physicalIdConnectionStatus.CONNECTION_DETAIL_FETCHING,
@@ -207,10 +233,7 @@ function isLoaderVisible(
   )
 }
 
-function getLoaderMessageText(
-  status: PhysicalIdProcessStatus
-) {
-
+function getLoaderMessageText(status: PhysicalIdProcessStatus) {
   switch (status) {
     case physicalIdProcessStatus.SDK_SCAN_START:
       return LOADER_TEXT.processing
@@ -289,7 +312,7 @@ function getErrorConnectionText(connectionStatus: PhysicalIdConnectionStatus) {
   }
 }
 
-const orderedDocuments = ["PASSPORT", "DRIVING_LICENSE", "IDENTITY_CARD"]
+const orderedDocuments = ['PASSPORT', 'DRIVING_LICENSE', 'IDENTITY_CARD']
 
 const documentTypesMap = {
   PASSPORT: 'Passport',
@@ -298,12 +321,12 @@ const documentTypesMap = {
 }
 
 const PhysicalIdDefault = ({
-                             country,
-                             setDocument,
-                             onCountrySelect,
-                             countryPickerVisible,
-                             setCountryPickerVisible,
-                           }) => {
+  country,
+  setDocument,
+  onCountrySelect,
+  countryPickerVisible,
+  setCountryPickerVisible,
+}) => {
   const dispatch = useDispatch()
   const documentTypes = useSelector(selectDocumentTypes)
   const documentTypesIsLoading = useSelector(selectDocumentTypesIsLoading)
@@ -315,10 +338,13 @@ const PhysicalIdDefault = ({
   }, [country])
 
   const data = useMemo(() => {
-    return documentTypes ?
-      orderedDocuments
-        .filter(document => documentTypes.includes(document))
-        .map(document => ({ label: documentTypesMap[document], value: document }))
+    return documentTypes
+      ? orderedDocuments
+          .filter((document) => documentTypes.includes(document))
+          .map((document) => ({
+            label: documentTypesMap[document],
+            value: document,
+          }))
       : []
   }, [documentTypes])
 
@@ -329,22 +355,22 @@ const PhysicalIdDefault = ({
   return (
     <>
       <View style={styles.containerStyles}>
-        {
-          !country ?
-            (
-              <View style={styles.containerStyles}>
-                <Image source={documentVerificationImage} style={styles.image}/>
-                <Text style={styles.physicalIdParagraphText}>
-                  By scanning your government-issued documents and taking a selfie, you can turn your physical documents into private, secure digital credentials stored on your phone. Your personal information is discarded after verification.
-                </Text>
-              </View>
-            ) :
-            (
-              <Text style={styles.physicalIdParagraphText}>
-                Choose a document to scan. If prompted, please grant camera permissions.
-              </Text>
-            )
-        }
+        {!country ? (
+          <View style={styles.containerStyles}>
+            <Image source={documentVerificationImage} style={styles.image} />
+            <Text style={styles.physicalIdParagraphText}>
+              By scanning your government-issued documents and taking a selfie,
+              you can turn your physical documents into private, secure digital
+              credentials stored on your phone. Your personal information is
+              discarded after verification.
+            </Text>
+          </View>
+        ) : (
+          <Text style={styles.physicalIdParagraphText}>
+            Choose a document to scan. If prompted, please grant camera
+            permissions.
+          </Text>
+        )}
         {countryPickerVisible || country ? (
           <CountryPicker
             withFilter={true}
@@ -358,33 +384,37 @@ const PhysicalIdDefault = ({
             countryCode={country || ''}
             visible={countryPickerVisible}
             onClose={onCloseCountryPicker}
-          />)  : null}
+          />
+        ) : null}
       </View>
       {country ? (
         <View style={styles.documentsContainer}>
-          { documentTypesIsLoading &&
-          <Loader showMessage={true} message="Getting supported document types" />
-          }
-          { !documentTypesIsLoading && (!data || data.length === 0) &&
-          <Text style={styles.errorText}>
-            There are no supported documents for this country
-          </Text>
-          }
-          { !documentTypesIsLoading && data && data.length > 0 &&
-          <RadioButton
-            data={data}
-            selectedBtn={setDocument}
-            icon={
-              <Icon name="check-circle" size={25} color={colors.green1} />
-            }
-            animationType="rotate"
-            duration={300}
-            textColor={colors.gray1}
-            activeColor={colors.green1}
-            boxActiveBgColor={colors.green3}
-            textStyle={styles.documentNameText}
-          />
-          }
+          {documentTypesIsLoading && (
+            <Loader
+              showMessage={true}
+              message="Getting supported document types"
+            />
+          )}
+          {!documentTypesIsLoading && (!data || data.length === 0) && (
+            <Text style={styles.errorText}>
+              There are no supported documents for this country
+            </Text>
+          )}
+          {!documentTypesIsLoading && data && data.length > 0 && (
+            <RadioButton
+              data={data}
+              selectedBtn={setDocument}
+              icon={
+                <Icon name="check-circle" size={25} color={colors.green1} />
+              }
+              animationType="rotate"
+              duration={300}
+              textColor={colors.gray1}
+              activeColor={colors.green1}
+              boxActiveBgColor={colors.green3}
+              textStyle={styles.documentNameText}
+            />
+          )}
         </View>
       ) : null}
     </>
@@ -394,7 +424,7 @@ const PhysicalIdDefault = ({
 const styles = StyleSheet.create({
   containerStyles: {
     flexDirection: 'column',
-    justifyContent: "space-around",
+    justifyContent: 'space-around',
     flex: 1,
     alignItems: 'center',
     marginBottom: 10,
