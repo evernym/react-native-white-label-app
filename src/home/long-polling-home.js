555527cd5d2374a04a539713db7f6020d9a3744f
Khagesh Sharma <khageshhiet@gmail.com>
2021-11-03 19:03:58 +0530

    Add polling as per different scenarios

--- src/home/long-polling-home.js
+++ src/home/long-polling-home.js
@@ -1,83 +1,74 @@
 // @flow
 
-import { all, put, race, select, take, call } from 'redux-saga/effects'
+import {
+  all,
+  put,
+  race,
+  select,
+  take,
+  call,
+  takeLeading,
+  takeLatest,
+} from 'redux-saga/effects'
 import delay from '@redux-saga/delay-p'
 import { AppState } from 'react-native'
 import { eventChannel } from 'redux-saga'
-import { homeDrawerRoute, homeRoute } from '../common'
+
 import { getUnacknowledgedMessages } from '../store/config-store'
-import { ROUTE_UPDATE } from '../store/route-store'
-import { getCurrentScreen } from '../store/store-selector'
 import {
   GET_MESSAGES_FAIL,
   GET_MESSAGES_LOADING,
   GET_MESSAGES_SUCCESS,
 } from '../store/type-config-store'
 import { pollingInterval } from '../external-imports'
+import {
+  CONNECTION_REQUEST_SENT,
+  NEW_CONNECTION_SUCCESS,
+} from '../store/type-connection-store'
+import { SEND_CLAIM_REQUEST_SUCCESS } from '../claim-offer/type-claim-offer'
+import { UPDATE_QUESTION_ANSWER } from '../question/type-question'
+import { PROOF_SUCCESS } from '../proof/type-proof'
 
-function* longPollHome(): any {
-  while (true) {
-    if (AppState.currentState !== 'active') {
-      // if app is not in foreground
-      // then wait for app to come to foreground
-      // we don't want to keep polling even if app is in background
-      const appStateChannel = yield call(appStateSource)
-      while (true) {
-        const state: string = yield take(appStateChannel)
-        if (state === 'active') {
-          // close channel, as we are not interested anymore to listen
-          // for state changes, since state has become 'active'
-          appStateChannel.close()
-          break
-        }
+const pollWithInterval = (interval: number) =>
+  function* poll(): any {
+    while (true) {
+      yield* ensureAppActive()
+
+      // we need to start a race
+      // among interval seconds or message download statues
+      // We are starting a race here because if before 15 seconds
+      // we receive a push notification, or user pull-refresh
+      // or user goes to some other screen, then we need to reset timer
+      // so essentially starting this while loop from start again
+      const [timeToRefresh] = yield race([
+        call(delay, interval),
+        take([GET_MESSAGES_LOADING, GET_MESSAGES_SUCCESS, GET_MESSAGES_FAIL]),
+      ])
+
+      if (timeToRefresh) {
+        console.log(`refresh after ${interval} seconds`)
+        // if 15 seconds are done, then start downloading messages
+        yield put(getUnacknowledgedMessages())
       }
     }
+  }
 
-    const currentRoute: string = yield select(getCurrentScreen)
-    if (!homeRoutes.includes(currentRoute)) {
-      // if we are not on home route, then wait for user to go to home route
-      yield take(isHomeRoute)
-      // as soon as we are on home screen, we need to start downloading messages
-      yield put(getUnacknowledgedMessages())
-    }
-
-    // now since we are on home route, we need to start a race
-    // among 15 seconds or user navigating to another screen
-    // or GET_MESSAGE_LOADING or GET_MESSAGE_SUCCESS
-    // We are starting a race here because if before 15 seconds
-    // we receive a push notification, or user pull-refresh
-    // or user goes to some other screen, then we need to reset timer
-    // and we need to wait for user to come back to home screen
-    // so essentially starting this loop from start again
-    const [timeToRefresh] = yield race([
-      call(delay, pollingInterval),
-      take(GET_MESSAGES_LOADING),
-      take(GET_MESSAGES_SUCCESS),
-      take(GET_MESSAGES_FAIL),
-      take(isNotHomeRoute),
-    ])
-
-    if (timeToRefresh) {
-      console.log('refresh after 15 seconds')
-      // if 15 seconds are done, then start downloading messages
-      yield put(getUnacknowledgedMessages())
-      // put to sleep for few seconds because we are triggering message download
-      // we don't want to put too much load on CAS
-      yield call(delay, pollingInterval)
+function* ensureAppActive(): any {
+  if (AppState.currentState !== 'active') {
+    // if app is not in foreground
+    // then wait for app to come to foreground
+    // we don't want to keep polling even if app is in background
+    const appStateChannel = yield call(appStateSource)
+    while (true) {
+      const state: string = yield take(appStateChannel)
+      if (state === 'active') {
+        // close channel, as we are not interested anymore to listen
+        // for state changes, since state has become 'active'
+        appStateChannel.close()
+        break
+      }
     }
   }
-}
-
-function isHomeRoute(action: *) {
-  return (
-    action.type === ROUTE_UPDATE && homeRoutes.includes(action.currentScreen)
-  )
-}
-
-function isNotHomeRoute(action: *) {
-  return (
-    action.type === ROUTE_UPDATE && !homeRoutes.includes(action.currentScreen)
-  )
 }
 
 function appStateSource() {
@@ -103,8 +94,39 @@ function appStateSource() {
   })
 }
 
-const homeRoutes = [homeRoute, homeDrawerRoute]
+function* startPolling(): any {
+  while (true) {
+    // first, we want to run polling with 2 seconds interval for 60 seconds
+    yield race([call(delay, 60), call(pollWithInterval(2))])
+
+    // we will exit above call after 60 seconds, because pollWithInterval function
+    // never returns, and hence race will end it's execution after 60 seconds delay is done
+
+    // now, we want to run polling with 3 seconds interval for 2 minutes
+    yield race([call(delay, 120), call(pollWithInterval(3))])
+
+    // Now we want to run polling normally with 15 seconds interval forever
+    yield* pollWithInterval(15)()
+  }
+}
+
+function* frequentPolling(): any {
+  // We are using takeLatest because we want to start polling again at 2 seconds interval
+  // if we would use takeLeading, then if saga were on 15 seconds interval,
+  // then it will keep on going for 15 seconds,
+  // but we want to stop previous timers and saga, and run a new one
+  yield takeLatest(
+    [
+      NEW_CONNECTION_SUCCESS,
+      CONNECTION_REQUEST_SENT,
+      UPDATE_QUESTION_ANSWER,
+      SEND_CLAIM_REQUEST_SUCCESS,
+      PROOF_SUCCESS,
+    ],
+    startPolling
+  )
+}
 
 export function* watchLongPollingHome(): any {
-  yield all([longPollHome()])
+  yield all([frequentPolling()])
 }
