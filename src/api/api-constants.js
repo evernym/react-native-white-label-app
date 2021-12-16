552ecfac4aec0ca9f0e484d1c9580d9677c30894
artem.ivanov <artem.ivanov@dsr-company.com>
2021-12-16 09:32:52 +0300

    Enable e2e tests

--- src/api/api-constants.js
+++ src/api/api-constants.js
@@ -36,7 +36,7 @@ export const MESSAGE_TYPE = {
   ARIES: 'aries',
   INVITE_ACTION: 'inviteAction',
   PRESENTATION_PROPOSAL: 'presentationProposal',
-  PROBLEM_REPORT: 'PROBLEM_REPORT'
+  PROBLEM_REPORT: 'PROBLEM_REPORT',
   UPGRADE: 'upgrade',
 }
 
